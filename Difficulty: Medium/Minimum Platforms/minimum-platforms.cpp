83af0f595a648ae1a9e669123261304ba2a83dd6
Ankit Kumar <149391333+Ankii04@users.noreply.github.com>
2026-01-19 23:54:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        sort(arr.begin(), arr.end());
+        sort(dep.begin(), dep.end());
+        int i = 0, j = 0;
+        int plat = 0, ans = 0;
+        int n = arr.size();
+        while(i < n && j < n) {
+            if(arr[i] <= dep[j]) {
+                plat++;
+                ans = max(ans, plat);
+                i++;
+            } else {
+                plat--;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
