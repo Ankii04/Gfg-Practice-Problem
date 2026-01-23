b2682a0265a7b18efee21282ea8a78b812755b0d
Ankit Kumar <149391333+Ankii04@users.noreply.github.com>
2026-01-24 02:56:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+  void find(int node,vector<vector<int>>&adj,vector<bool>&visited,vector<int>&ans)
+  {
+      if(visited[node])
+      {
+          return;
+      }
+      
+      visited[node]=1;
+      ans.push_back(node);
+      
+      
+      for(int i=0;i<adj[node].size();i++)
+      {
+          find(adj[node][i],adj,visited,ans);
+      }
+  }
+    
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        vector<bool>visited(adj.size(),0);
+        vector<int>ans;
+        
+        find(0,adj,visited,ans);
+        return ans;
+    }
+};
