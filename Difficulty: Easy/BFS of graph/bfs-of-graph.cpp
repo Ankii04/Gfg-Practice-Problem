bfdda63e55c98e018577d928b034024dcc4ff4b4
Ankit Kumar <149391333+Ankii04@users.noreply.github.com>
2026-01-24 02:00:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        vector<int>ans;
+        vector<bool>visited(adj.size(),0);
+        
+        queue<int>q;
+        q.push(0);
+        visited[0]=1;
+        
+        int node;
+        while(!q.empty())
+        {
+            node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(int i=0;i<adj[node].size();i++)
+            {
+                if(!visited[adj[node][i]])
+                {
+                    visited[adj[node][i]]=1;
+                    q.push(adj[node][i]);
+                }    
+            }
+        }
+        return ans;
+    }
+};
